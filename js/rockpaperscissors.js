df7d881b7cdb585cf423c72a310a12a41578dda9
Justin Pillay <jpillay07@gmail.com>
2022-06-14 00:18:14 +0200

    JS logic behinf the rock paper scissors game added

--- js/rockpaperscissors.js
+++ js/rockpaperscissors.js
@@ -3,6 +3,17 @@
 //create variable to store the computer's score and the players score as well as strings for rock, paper and scissors
 let computerScore = 0;
 let playerScore = 0;
+let rounds = 0;
+
+//Create variables to store text output of respective scores
+const playerScoreText = document.getElementById("player-score");
+const computerScoreText = document.getElementById("computerScore");
+
+//Create variable to store middle text
+const middleText = document.getElementById("middle-text");
+
+//Create variable to store round result p element
+const roundResult = document.getElementById("round-result");
 
 const ROCK = "rock";
 const PAPER = "paper";
@@ -17,6 +28,27 @@ const scissorsButton = document.getElementById("scissors-button");
 const playAgainButton = document.getElementById("play-again-button");
 const playAgainButtonImg = document.getElementById("play-again-img");
 
+//The following events call the playRound function and passes the relevant string based on the players button selection
+rockButton.addEventListener("click", () => {
+
+    playRound(ROCK, computerSelect());
+
+});
+
+paperButton.addEventListener("click", () => {
+
+    playRound(PAPER, computerSelect());
+
+});
+
+scissorsButton.addEventListener("click", () => {
+
+    playRound(SCISSORS, computerSelect());
+
+});
+
+
+//Restart the game and reset variables
 playAgainButton.addEventListener("click", () =>{
 
     //Change the button image when clicked
@@ -24,8 +56,10 @@ playAgainButton.addEventListener("click", () =>{
 
     setTimeout(function(){
         playAgainButtonImg.src = "/images/PlayAgain1.png";
+        resetGame();
     }, 200);
 
+    
     
 });
 
@@ -33,7 +67,9 @@ playAgainButton.addEventListener("click", () =>{
 //Create a function called computerSelect that randomly selects rock, paper or scissors and returns the selection in a string
 function computerSelect(){
     //Generate a number from 1 to 3 (1 = rock, 2 = paper and 3 = scissors and then based on the number, return the relevant string)
-    let choice = (Math.random() * 3) + 1;
+    let choice = Math.floor(Math.random() * 3 + 1);
+
+    console.log(choice);
 
     if(choice === 1){
         return ROCK;
@@ -49,84 +85,83 @@ function computerSelect(){
 //this function plays one round and compares the player input vs the computer input and determines the winner of the round
 function playRound(playerSelection, computerSelection){
 
+    //Increment the number of rounds played
+    rounds++;
+
     //if the player and the computer have the same choice, its a tie, otherwise compare each choice, update the score and determine the winner
     if(playerSelection === computerSelection){
-        return `Player Selection: ${playerSelection}, Computer Selection: ${computerSelection} - It's a tie!`;
+        roundResult.innerText = `Player Selection: ${playerSelection}, Computer Selection: ${computerSelection} - It's a tie!`;
     }
     else if(playerSelection === ROCK && computerSelection === PAPER){
         computerScore++;
-        return `Player Selection: ${playerSelection}, Computer Selection: ${computerSelection} - Computer wins!`;
+        computerScoreText.innerText = computerScore;
+        roundResult.innerText = `Player Selection: ${playerSelection}, Computer Selection: ${computerSelection} - Computer wins!`;
     }
     else if(playerSelection === ROCK && computerSelection === SCISSORS){
         playerScore++;
-        return `Player Selection: ${playerSelection}, Computer Selection: ${computerSelection} - You win!`;
+        playerScoreText.innerText = playerScore;
+        roundResult.innerText = `Player Selection: ${playerSelection}, Computer Selection: ${computerSelection} - You win!`;
     }
     else if(playerSelection === PAPER && computerSelection === ROCK){
         playerScore++;
-        return `Player Selection: ${playerSelection}, Computer Selection: ${computerSelection} - You win!`;
+        playerScoreText.innerText = playerScore;
+        roundResult.innerText = `Player Selection: ${playerSelection}, Computer Selection: ${computerSelection} - You win!`;
     }
     else if(playerSelection === PAPER && computerSelection === SCISSORS){
         computerScore++;
-        return `Player Selection: ${playerSelection}, Computer Selection: ${computerSelection} - Computer wins!`;
+        computerScoreText.innerText = computerScore;
+        roundResult.innerText = `Player Selection: ${playerSelection}, Computer Selection: ${computerSelection} - Computer wins!`;
     }
     else if(playerSelection === SCISSORS && computerSelection === ROCK){
         computerScore++;
-        return `Player Selection: ${playerSelection}, Computer Selection: ${computerSelection} - Computer wins!`;
+        computerScoreText.innerText = computerScore;
+        roundResult.innerText = `Player Selection: ${playerSelection}, Computer Selection: ${computerSelection} - Computer wins!`;
     }
     else if(playerSelection === SCISSORS && computerSelection === PAPER){
         playerScore++;
-        return `Player Selection: ${playerSelection}, Computer Selection: ${computerSelection} - You win!`;
+        playerScoreText.innerText = playerScore;
+        roundResult.innerText = `Player Selection: ${playerSelection}, Computer Selection: ${computerSelection} - You win!`;
     }
-}
 
-//This function checks the user input
-function checkBadInput(input){
-    if(input === ROCK){
-        return false;
-    }
-    else if(input === SCISSORS){
-        return false;
-    }
-    else if(input === PAPER){
-        return false;
-    }
-    else{
-        return true;
-    }
-}
-
-//This funtion plays a total of 5 rounds and determines the winner at the end
-function game(){
-    let userChoice = "";
-    let roundResult = "";
-    console.log("Rock, Paper, Scissors! It's you vs the computer. Enter your choice (Rock, Paper or Scissors");
-
-    //loop through 5 games
-    for(let i = 0; i < 5; i++){
-        userChoice = prompt("Enter your choice.");
-        userChoice = userChoice.toLowerCase();
-
-        //Check if the user's input is valid. If not, prompt the user until a valid input is given
-        while(checkBadInput(userChoice)){
-            alert("Please enter either rock, paper or scissors (check your spelling).");
-            userChoice = prompt("Enter your choice.");
-            userChoice = userChoice.toLowerCase();
+    if(rounds === 5){
+        roundResult.innerText = "";
+        disableButtons();
+        playAgainButton.style.visibility = "visible";
+        if(computerScore > playerScore){
+            middleText.innerText = "Computer Wins!";
         }
-        
-        //Play one round of the game
-        roundResult = playRound(userChoice, computerSelect());
-        console.log(roundResult);
+        else if(computerScore < playerScore){
+            middleText.innerText = "You Win!";
+        }
+        else{
+            middleText.innerText = "It's a Draw!";
+        }
     }
+}
 
-    //Determine the winner
-    if(playerScore > computerScore){
-        console.log("You win the game!");
-    }
-    else if(playerScore < computerScore){
-        console.log("You lose :-(");
-    }
-    else{
-        console.log("It's a tie game!")
-    }
+function disableButtons(){
+    rockButton.disabled = true;
+    paperButton.disabled = true;
+    scissorsButton.disabled = true;
+}
 
+function enableButtons(){
+    rockButton.disabled = false;
+    paperButton.disabled = false;
+    scissorsButton.disabled = false;
+}
+
+function resetGame(){
+    computerScore = 0;
+    playerScore = 0;
+    rounds = 0;
+
+    enableButtons();
+
+    middleText.innerText = "VS";
+
+    computerScoreText.innerText = "0";
+    playerScoreText.innerText = "0";
+
+    playAgainButton.style.visibility = "hidden";
 }
